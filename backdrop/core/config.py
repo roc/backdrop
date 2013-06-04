3795175940b18c2b81c06bb5a5577943626a0f4f
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-06-04 19:30:27 +0100

    Python 2.6 compatibility bug
    Prior to 2.7 indexes are required.

--- backdrop/core/config.py
+++ backdrop/core/config.py
@@ -14,8 +14,8 @@ CONFIG_PATH = os.getenv('BACKDROP_CONFIG_ROOT',
 
 def load(app, name):
     """Load configuration for a given flask app"""
-    load_config_file(app, '{}.py'.format(GOVUK_ENV))
-    load_config_file(app, '{}/{}.py'.format(name, GOVUK_ENV))
+    load_config_file(app, '{0}.py'.format(GOVUK_ENV))
+    load_config_file(app, '{0}/{1}.py'.format(name, GOVUK_ENV))
 
     load_legacy_config_file(app, name, GOVUK_ENV)
 
@@ -30,6 +30,6 @@ def load_config_file(app, path):
 
 def load_legacy_config_file(app, name, env):
     """Load a config file from the legacy location if it exists"""
-    path = os.path.join(ROOT_PATH, 'backdrop', name, '{}.py'.format(env))
+    path = os.path.join(ROOT_PATH, 'backdrop', name, '{0}.py'.format(env))
     if os.path.exists(path):
-        app.config.from_object('backdrop.{}.{}'.format(name, env))
+        app.config.from_object('backdrop.{0}.{1}'.format(name, env))
